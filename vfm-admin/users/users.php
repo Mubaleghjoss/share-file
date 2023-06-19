a449b5dfb4326c27bbf8b8f985ebe8844799aebb
mubelghjoss <kamulyan1996@gmai.com>
2023-06-19 20:39:15 +0700

    firts commit

---
+++ vfm-admin/users/users.php
@@ -0,0 +1,18 @@
+<?php
+
+ $_USERS = array (
+  0 => 
+  array (
+    'name' => 'admin',
+    'pass' => '$1$kI4qrBAC$iemEwwyWJLoNBfeRfj5Zv/',
+    'role' => 'superadmin',
+    'email' => 'smaafbs@sch.id',
+  ),
+  1 => 
+  array (
+    'name' => 'putra',
+    'pass' => '$1$aooH5qKC$eedI08pHn80tR15h8M97P.',
+    'role' => 'superadmin',
+    'email' => 'kamulyan1996@gmail.com',
+  ),
+);
